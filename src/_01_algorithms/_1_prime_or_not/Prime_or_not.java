ad906bbf7e2b8079426498c794f2bc339a8f2193
league <league@imac-34.attlocal.net>
2022-12-16 18:26:53 -0800

    asdfasdf

--- src/_01_algorithms/_1_prime_or_not/Prime_or_not.java
+++ src/_01_algorithms/_1_prime_or_not/Prime_or_not.java
@@ -1,10 +1,14 @@
 package _01_algorithms._1_prime_or_not;
 
+import javax.swing.JOptionPane;
+
 public class Prime_or_not {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String numberAsString = JOptionPane.showInputDialog("Give me a number");
+		int number = Integer.parseInt(numberAsString);
+		if(number %% )
 	}
 
 }
